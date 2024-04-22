ec3a2117e01757cb50d4d2e6fe506111cff6d1f5
astitva-exe-23 <110301422+astitva-exe-23@users.noreply.github.com>
2024-04-22 23:20:10 +0530

    Update Delivery.js

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -5,7 +5,7 @@ const Delivery = () => {
     <div className='w-full bg-white py-16 px-4'>
         <h3 className='text-orange-500  font-bold text-2xl text-center'>Quick Delivery App</h3>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-            <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp'/>
+            <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp' alt="deliver"/>
             <div className='flex flex-col justify-center'>
                 <p className='text-[#00df9a] font-bold'>Get The App</p>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-demand</h1>
